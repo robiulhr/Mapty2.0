0d5f3c9ca0a7fd094e6c58b1c01b7395ec688d48
Robiul H <robiul@robiul.robiul.dev>
2023-02-21 10:24:52 +0600

    bug: removing the border of chart div

--- src/js/main.js
+++ src/js/main.js
@@ -286,22 +286,6 @@
     }
 
     _init(dataType, savedData, currentPage) {
-      const chartAreaBorder = {
-        id: "chartAreaBorder",
-        beforeDraw(chart, args, options) {
-          const {
-            ctx,
-            chartArea: { left, top, width, height },
-          } = chart;
-          ctx.save();
-          ctx.strokeStyle = options.borderColor;
-          ctx.lineWidth = options.borderWidth;
-          ctx.setLineDash(options.borderDash || []);
-          ctx.lineDashOffset = options.borderDashOffset;
-          ctx.strokeRect(left, top, width, height);
-          ctx.restore();
-        },
-      };
       const customCanvasBackgroundColor = {
         id: "customCanvasBackgroundColor",
         beforeDraw: (chart, args, options) => {
@@ -425,7 +409,7 @@
             },
           },
         },
-        plugins: [chartAreaBorder, customCanvasBackgroundColor],
+        plugins: [customCanvasBackgroundColor],
       });
     }
     _updateChartForAllType() {
